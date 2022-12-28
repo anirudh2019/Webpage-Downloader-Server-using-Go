eef10faf118d7a455b23aaa53563d95986104a74
anirudh2019 <iit2019068@iiita.ac.in>
2022-12-28 17:25:07 +0530

    added HTTP handler, parsing the request payload

--- server.go
+++ server.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
 // PageRequest represents the request payload that the endpoint expects.
 type PageRequest struct {
 	URI        string `json:"uri"`
@@ -11,4 +17,18 @@ type PageResponse struct {
 	ID        string `json:"id"`
 	URI       string `json:"uri"`
 	SourceURI string `json:"sourceUri"`
+}
+
+func main() {
+	http.HandleFunc("/pagesource", func(w http.ResponseWriter, r *http.Request) {
+		// Parse the request payload.
+		var req PageRequest
+		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+			http.Error(w, "Invalid request payload", http.StatusBadRequest)
+			return
+		}
+
+	})
+	log.Println("Starting server on port 7771...")
+	log.Fatal(http.ListenAndServe(":7771", nil))
 }
