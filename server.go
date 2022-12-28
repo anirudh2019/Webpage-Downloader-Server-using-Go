683f05164de49087d4e169e9cb1c443c3406289d
anirudh2019 <iit2019068@iiita.ac.in>
2022-12-28 17:58:03 +0530

    fetches webpage and download as file

--- server.go
+++ server.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
 // PageRequest represents the request payload that the endpoint expects.
@@ -21,6 +24,11 @@ type PageResponse struct {
 
 func main() {
 	http.HandleFunc("/pagesource", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+			return
+		}
+
 		// Parse the request payload.
 		var req PageRequest
 		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
@@ -28,6 +36,36 @@ func main() {
 			return
 		}
 
+		// Fetch the webpage and download it as a file.
+		var err error
+		var resp *http.Response
+		for i := 0; i < req.RetryLimit; i++ {
+			resp, err = http.Get(req.URI)
+			if err == nil {
+				break
+			}
+			time.Sleep(time.Second)
+		}
+		defer resp.Body.Close()
+
+		// Generate a unique ID for the file.
+		id := fmt.Sprintf("%x", time.Now().UnixNano())
+
+		// Save the file to the local file system.
+		file, err := os.Create(fmt.Sprintf("/files/%s.html", id))
+		defer file.Close()
+
+		// Return the response payload.
+		res := PageResponse{
+			ID:        id,
+			URI:       req.URI,
+			SourceURI: fmt.Sprintf("/files/%s.html", id),
+		}
+
+		if err := json.NewEncoder(w).Encode(res); err != nil {
+			http.Error(w, "Failed to encode response", http.StatusInternalServerError)
+			return
+		}
 	})
 	log.Println("Starting server on port 7771...")
 	log.Fatal(http.ListenAndServe(":7771", nil))
